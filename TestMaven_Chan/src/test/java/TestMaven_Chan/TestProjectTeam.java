7ba3d900761fb4c5e63ba8a1f0b6281a8d980f2b
Chandan <Chandanvelamala@gmail.com>
2020-10-12 21:36:00 -0400

    Maven Project Push

---
+++ TestMaven_Chan/src/test/java/TestMaven_Chan/TestProjectTeam.java
@@ -0,0 +1,13 @@
+package TestMaven_Chan;
+
+import org.testng.annotations.Test;
+
+public class TestProjectTeam {
+	
+	@Test
+	public void TestProjectTeam() {
+		
+		System.out.println("... Hi Team ...!");
+	}
+
+}
